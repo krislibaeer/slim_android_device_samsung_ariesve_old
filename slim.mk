e1e949a1fed94e307598af2bfc59ca524c056f71
krislibaeer <krislibaeer@googlemail.com>
2013-06-04 17:10:27 +0200

    changes for slimrom
    Change-Id: I58d63391a492bd0e1fe31a70dc9c529d6539f888
    
    Conflicts:
    	overlay/packages/apps/Settings/res/values/config.xml
    	slim.mk

--- slim.mk
+++ slim.mk
@@ -1,20 +1,27 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/slim/config/gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, $(LOCAL_PATH)/full_ariesve.mk)
+$(call inherit-product, device/samsung/ariesve/full_ariesve.mk)
+
+# Inherit Device Settings
+$(call inherit-product, vendor/slim/config/common_sgs_plus.mk)
 
 PRODUCT_RELEASE_NAME := ariesve
 
 # Setup device configuration
-PRODUCT_NAME := cm_ariesve
+PRODUCT_NAME := slim_ariesve
 PRODUCT_DEVICE := ariesve
 PRODUCT_BRAND := Samsung
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := GT-I9001
+
+#Boot Animation
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=samsung/GT-I9001/GT-I9001:2.3.6/GINGERBREAD/XXKQN:user/release-keys PRIVATE_BUILD_DESC="GT-I9001-user 2.3.6 GINGERBREAD XXKQN release-keys"
 
